3693f13ed13ac75d74a84a0b47274c68d51305fd
Sergio Arribas <arribastellezsergio@gmail.com>
2024-08-26 19:21:49 +0000

    To Do

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      setTodos([...todos, inputValue.trim()]);
+      setInputValue(""); // Limpiar el campo de entrada
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>My To Do List</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={handleKeyDown}
+            placeholder="What do you need to do?"
+          />
+        </li>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <i className="fa-solid fa-trash" />
+          </li>
+        ))}
+      </ul>
+      <div>{todos.length} Tasks</div>
+    </div>
+  );
 };
 
 export default Home;
